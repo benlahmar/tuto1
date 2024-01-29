658cad849a038a2c22d97f118a1e4b7728d2de81
h.benlahmer <h.benlahmer@gmail.com>
2024-01-29 12:55:31 +0100

    add

---
+++ tuto1/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+	@GetMapping("/abc/{p}")
+	public String hi(@PathVariable String p)
+	{
+		return "hello Mr"+p;
+	}
+}
---
+++ tuto1/src/main/java/com/example/demo/Tuto1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuto1Application.class, args);
+	}
+
+}
